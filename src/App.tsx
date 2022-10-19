9a1720f09821016b9f502a6e1c9d95ddb326cc2b
crespire <li.simmon@gmail.com>
2022-10-19 16:37:31 -0400

    feat: scaffolds application
    * Adds and configures TailwindCSS
    * Adds React Router
    * Configures Router
    * Scaffolds Application Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-function App() {
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+
+// Template for Site
+const App: FC = () => {
   return (
     <div className="App">
-      App
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
