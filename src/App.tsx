7f986505f92eaf04b8ac541500056e810f2d2b11
crespire <li.simmon@gmail.com>
2022-10-21 15:18:14 -0400

    fix: CSS dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Hero from './components/Hero';
 // Main Site
 const App: FC = () => {
   return (
-    <div className="container mx-auto h-full max-w-prose px-4">
+    <div className="container mx-auto h-full max-w-prose px-4 bg-pale text-coffee dark:text-pale dark:bg-coffee">
       <Header />
       <main>
         <Hero />
