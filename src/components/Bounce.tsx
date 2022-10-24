440e1d23d5175d41cc54b6e6e61d5f44fbcfd369
crespire <li.simmon@gmail.com>
2022-10-24 13:54:46 -0400

    fix: adjust bouncer size

--- src/components/Bounce.tsx
+++ src/components/Bounce.tsx
@@ -3,7 +3,7 @@ import ArrowIcon from '../assets/arrow-down.png';
 
 const Bounce: FC = () => {
   return (
-    <div className='relative top-40 opacity-25 animate-bounce'>
+    <div className='relative max-w-[2rem] top-40 opacity-25 animate-bounce'>
       <img className="dark:invert" src={ArrowIcon} alt="&darr;" />
     </div>
   );
