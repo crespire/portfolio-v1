6cf8943b373078ad9e360c26227556977b6483d0
crespire <li.simmon@gmail.com>
2022-10-24 15:11:41 -0400

    fix: update About header

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const About: FC = () => {
   return (
     <section id="about" className="flex flex-col leading-relaxed sm:leading-10 justify-center items-start h-screen space-y-4">
-      <h1 className="font-semibold text-[3rem] sm:text-[4rem] text-tangerine">About Me</h1>
+      <h1 className="font-semibold text-[3rem] sm:text-[4rem] text-tangerine text-center w-full">About Me</h1>
       <p>
         My name is Simmon Li and I love creating things from Pong to fullstack applications. Since September 2021, I've been learning via <a className="underline" href="https://theodinproject.com">The Odin Project</a>. I'm currently an operations professional, but looking for an opportunity to jump into a career in software development.
       </p>
