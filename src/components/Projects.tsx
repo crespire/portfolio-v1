e3bc4e7c3d645829b847cc16fee024f20b79d5d7
crespire <li.simmon@gmail.com>
2022-10-24 19:24:24 -0400

    fix: adjust mobile project borders

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ const Projects: FC = () => {
   return (
     <section id="projects" className="grow min-h-screen h-fit pt-[3rem]">
       <h1 className="font-semibold text-[3rem] sm:text-[4rem] w-prose text-center text-salmon">Projects</h1>
-      <div className="flex flex-wrap justify-center align-center space-2 grow h-fit">        
+      <div className="flex flex-wrap justify-center align-center space-2 grow h-fit gap-5">
         { projects.map(item => {
           return <ProjectCard key={item.key} project={item} />
         })}
