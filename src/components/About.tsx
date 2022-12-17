75914e0a925dab03693de904858e2d40a7e6a8d7
crespire <li.simmon@gmail.com>
2022-12-17 18:05:01 -0500

    fix: spacing of elements in landscape mode

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,22 +2,24 @@ import { FC } from 'react';
 
 const About: FC = () => {
   return (
-    <section id="about" className="flex flex-col max-w-prose leading-relaxed sm:leading-10 justify-center items-start h-screen space-y-4">
-      <h1 className="font-semibold text-[3rem] sm:text-[4rem] text-tangerine text-center w-full">About Me</h1>
-      <p>
-        My name is Simmon Li and I love creating things from Pong to fullstack applications. Since September 2021, I've been learning via <a className="underline" href="https://theodinproject.com">The Odin Project</a>. I'm currently an operations professional, but looking for an opportunity to jump into a career in software development.
-      </p>
-      <span className="w-full">
-        <h3 className="text-[1.25rem]">Current Skills</h3>
-        <ul className="grid grid-cols-2 gap-1 sm:gap-3 list-disc list-inside">
-          <li>Ruby</li>
-          <li>Ruby on Rails</li>
-          <li>Javascript</li>
-          <li>Typescript</li>
-          <li>React</li>
-          <li>Node.js</li>
-        </ul>
-      </span>      
+    <section id="about" className="grow min-h-screen h-fit landscape:pt-[5rem]">
+      <div className="flex flex-col max-w-prose leading-relaxed sm:leading-10 justify-center items-start h-screen space-y-4">
+        <h1 className="font-semibold text-[3rem] sm:text-[4rem] text-tangerine text-center w-full">About Me</h1>
+        <p>
+          My name is Simmon Li and I love creating things from Pong to fullstack applications. Since September 2021, I've been learning via <a className="underline" href="https://theodinproject.com">The Odin Project</a>. I'm currently an operations professional, but looking for an opportunity to jump into a career in software development.
+        </p>
+        <span className="w-full">
+          <h3 className="text-[1.25rem]">Current Skills</h3>
+          <ul className="grid grid-cols-2 gap-1 sm:gap-3 list-disc list-inside">
+            <li>Ruby</li>
+            <li>Ruby on Rails</li>
+            <li>Javascript</li>
+            <li>Typescript</li>
+            <li>React</li>
+            <li>Node.js</li>
+          </ul>
+        </span>
+      </div>
     </section>
   );
 };
--- src/components/Bounce.tsx
+++ src/components/Bounce.tsx
@@ -3,7 +3,7 @@ import ArrowIcon from '../assets/arrow-down.png';
 
 const Bounce: FC = () => {
   return (
-    <div className='relative max-w-[2rem] top-40 opacity-40 animate-bounce'>
+    <div className='relative max-w-[2rem] top-40 opacity-40 animate-bounce landscape:top-5'>
       <img className="dark:invert" src={ArrowIcon} alt="&darr;" />
     </div>
   );
