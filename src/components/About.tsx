f22cf9cdcf7b24b8b783bc42138593082c28b38b
crespire <li.simmon@gmail.com>
2022-12-21 09:25:13 -0500

    fix: update about blurb

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About: FC = () => {
       <div className="flex flex-col max-w-prose leading-relaxed sm:leading-10 justify-center items-start h-screen space-y-4">
         <h1 className="font-semibold text-[3rem] sm:text-[4rem] text-tangerine text-center w-full">About Me</h1>
         <p>
-          My name is Simmon Li and I love creating things from Pong to fullstack applications. Since September 2021, I've been learning via <a className="underline" href="https://theodinproject.com">The Odin Project</a>. I'm currently an operations professional, but looking for an opportunity to jump into a career in software development.
+          I love working through a problem step by step and solving it. I've got a bias for action and I'm not afraid to fail (in a feature branch, of course). I'm a self-taught developer with a proven record of continuous self-learning and improvement. You can see my journey for yourself in my Github history. <a className="underline" href="https://github.com/crespire">Take a look</a>, and consider putting my drive and passion to work for your team.
         </p>
         <span className="w-full">
           <h3 className="text-[1.25rem]">Current Skills</h3>
