0054ec9187ddc77a9265d52f62a4c14d505f2ed0
crespire <li.simmon@gmail.com>
2022-12-21 09:36:01 -0500

    fix: update about and add button styling to footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -13,9 +13,9 @@ const Footer: FC = () => {
   return (
     <footer className="fixed left-0 bottom-0 w-full p-2 flex justify-center items-center bg-pale dark:bg-coffee">
       <section className="w-full max-w-prose flex justify-around items-center">
-        <a href="https://github.com/crespire">Github</a>
-        <a href="https://linkedin.com/in/simmonli">LinkedIn</a>
-        <button id="emailme">Email</button>
+        <a className="border border-solid p-1" href="https://github.com/crespire">Github</a>
+        <a className="border border-solid p-1" href="https://linkedin.com/in/simmonli">LinkedIn</a>
+        <button id="emailme" className="border border-solid p-1">Email</button>
       </section>      
     </footer>
   );
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About: FC = () => {
       <div className="flex flex-col max-w-prose leading-relaxed sm:leading-10 justify-center items-start h-screen space-y-4">
         <h1 className="font-semibold text-[3rem] sm:text-[4rem] text-tangerine text-center w-full">About Me</h1>
         <p>
-          I love working through a problem step by step and solving it. I've got a bias for action and I'm not afraid to fail (in a feature branch, of course). I'm a self-taught developer with a proven record of continuous self-learning and improvement. You can see my journey for yourself in my Github history. <a className="underline" href="https://github.com/crespire">Take a look</a>, and consider putting my drive and passion to work for your team.
+          I love working through a problem step by step and solving it. I've got a bias for action and I'm not afraid to fail (in a feature branch, of course). I'm a self-taught developer with a proven record of continuous self-learning and improvement. Feel free to take a look at <a className="underline" href="https://github.com/crespire">my Github history</a> and consider putting my drive and passion to work for your team.
         </p>
         <span className="w-full">
           <h3 className="text-[1.25rem]">Current Skills</h3>
