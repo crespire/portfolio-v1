492ff64c9444a6a57c3901d5884979e9399f8c7c
crespire <hello@crespire.dev>
2023-03-08 11:00:32 -0500

    feat: update Hero page
    * Dial down the text size to leave more room for a blurb
    * Added blurb to hero page.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,15 @@ import Bounce from './Bounce';
 
 const Hero: FC = () => {
   return (
-    <section id="hero" className="flex flex-col justify-center items-center h-screen">
+    <section id="hero" className="flex flex-col justify-center items-center h-screen max-w-prose">
       <span>
-        Nice to meet you! I'm
+        Hi, my name is
         <h1 className="font-semibold text-dynamic-large text-lake">Simmon Li</h1>
-        <h3 className="italic text-dynamic-small">I love building things with code.</h3>
+        <h3 className="italic text-dynamic-small">Budding full stack Rails developer.</h3>
+        <p>
+          Driven and independent with a high aptitude for learning, I strive for excellence while building software. I communicate effectively around technical topics and love teaching and talking about tech.
+
+        </p>
       </span>
       <Bounce />
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,9 @@ module.exports = {
         "barlow": ['Barlow', 'sans-serif']
       },
       fontSize: {
-        "dynamic-large": "clamp(3rem, 1.0435rem + 8.6957vw, 8rem)",
-        "dynamic-small": "clamp(1.25rem, 0.3696rem + 3.9130vw, 3.5rem)"
+        //"dynamic-large": "clamp(3rem, 1.0435rem + 8.6957vw, 8rem)",
+        "dynamic-large": "clamp(3rem, 0.4rem + 4vw, 3.5rem)",
+        "dynamic-small": "clamp(1.25rem, 0.2rem + 2vw, 1.5rem)"
       },
     },
   },
